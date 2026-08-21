61b9890ec8293960badcaf0bac213ab2433ac25a
chromehaert <7xvnsz@gmail.com>
2026-08-21 14:48:35 +0700

    a

--- src/utils/fog.h
+++ src/utils/fog.h
@@ -2,7 +2,7 @@
 #define FOG_UTIL_H
 
 float calculateFog(float relativeDepth, vec4 fogDistCtrl) {
-  return clamp((((relativeDepth / fogDistCtrl.z) - (0.15 + fogDistCtrl.x)) / (0.15 + (fogDistCtrl.y - fogDistCtrl.x))), 0.0, 1.0);
+  return clamp((((relativeDepth / fogDistCtrl.z) - (fogDistCtrl.x)) / ((fogDistCtrl.y - fogDistCtrl.x))), 0.0, 1.0);
 }
 
 #endif // FOG_UTIL_H
